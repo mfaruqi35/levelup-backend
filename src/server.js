9089eb99459c283e66f163c6c641fcc647035121
mfaruqi35 <muhammad.faruqi35@gmail.com>
2025-10-17 20:26:50 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import mongoose from "mongoose";
+import { configDotenv } from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+configDotenv();
+
+const app = express()
+const PORT = process.env.PORT
+
+mongoose.connect(process.env.MONGODB_URI)
+
+const db = mongoose.connection;
+db.on("Error", (error) => console.error(error));
+db.once("Open", () => console.log("Connected to MongoDB"));
+
+app.use(express.json())
+app.use(cors({ origin: "*", optionsSuccessStatus: 200}));
+
+app.use(bodyParser.json());
+
+app.listen(PORT, () => console.log(`MongoDB berhasil terkoneksi pada port: ${PORT}`))
