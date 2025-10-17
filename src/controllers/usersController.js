ff2104c3d4eb8f0394a5c8bfd45a47b18c406bc0
mfaruqi35 <muhammad.faruqi35@gmail.com>
2025-10-17 21:46:35 +0700

    feat: auth

---
+++ src/controllers/usersController.js
@@ -0,0 +1,92 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import User from "../models/usersModel.js";
+
+export const registerUser = async (req, res) => {
+    try {
+        const { fullname, email, password, role } = req.body;
+        if (!fullname || !email || !password ) {
+            res.status(400).json({ 
+                message: "Mohon isi semua field yang diperlukan", 
+                status: 400, 
+                data: null });
+        }
+
+        const isAlreadyRegister = await User.findOne({ email })
+        if (isAlreadyRegister){
+            res.status(400).json({ 
+                message: "Pengguna dengan email ini telah terdaftar", 
+                status: 400, 
+                data: null});
+        }
+        else {
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(password, salt);
+            const newUser = new User({
+                fullname,
+                email,
+                password: hashedPassword,
+                role,
+            });
+            await newUser.save();
+            return res.status(201).json({ 
+                message: "Pengguna berhasil didaftar ",
+                status: 201,
+                data: newUser
+            });
+        }
+
+    } catch(error) {
+        return res.status(500).json({ message: error.message});
+    }
+}
+
+export const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            res.status(400).json({ 
+                message: "Tolong isi field yang diperlukan",
+                status:400,
+                data: null});
+        }
+
+        const user = await User.findOne({ email:email });
+        if (!user) {
+            res.status(400).json({ 
+                message: "User tidak ditemukan",
+                status: 404,
+                data: null
+            });
+        } 
+            
+        const validateUser = await bcrypt.compare(password, user.password);
+        if (!validateUser){
+            res.status(400).json({ message: "Password salah, coba lagi", status: 400, data: null});
+        }
+            
+        const payload = {
+            userId: user._id,
+            email: user.email,
+            role: user.role,
+        };
+
+        const JWT_SECRET = process.env.JWT_SECRET
+        if (!JWT_SECRET) {
+            console.error("JWT_SECRET tidak terdefinisi di file .env");
+            return res.status(500).json({ message: "Kesalahan konfigurasi server." });
+        }
+                    
+        const token = jwt.sign(payload, JWT_SECRET, {expiresIn: "1d"});
+                    
+        return res.status(200).json({
+            message: "Login berhasil",
+            status: 200,
+            data: user,
+            token: token
+        });
+
+    } catch(error) {
+        res.status(500).json({ message: error.message });
+    }
+}
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import { configDotenv } from "dotenv";
 import cors from "cors";
 import bodyParser from "body-parser";
+import userRouter from "./routes/userRoute.js";
 
 configDotenv();
 
@@ -17,7 +18,8 @@ db.once("Open", () => console.log("Connected to MongoDB"));
 
 app.use(express.json())
 app.use(cors({ origin: "*", optionsSuccessStatus: 200}));
-
 app.use(bodyParser.json());
+
+app.use("/user", userRouter)
 
 app.listen(PORT, () => console.log(`MongoDB berhasil terkoneksi pada port: ${PORT}`))
